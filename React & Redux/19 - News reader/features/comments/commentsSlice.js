1a4b8fd01d7099f848ef7fb97aa6079bcae52e82
Idris Patel <81174101+ipa1987@users.noreply.github.com>
2022-01-08 21:53:44 +0000

    Add files via upload

---
+++ React & Redux/19 - News reader/features/comments/commentsSlice.js
@@ -0,0 +1,28 @@
+// Import createAsyncThunk and createSlice here.
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+// Create loadCommentsForArticleId here.
+const loadCommentsForArticleId = createAsyncThunk(
+  'comments/loadCommentsForArticleId',
+  async (id) => {
+    const response = await fetch('api/articles/${id}/comments');
+    const json = await response.json();
+    return json;
+  }
+)
+
+// Create postCommentForArticleId here.
+
+export const commentsSlice = createSlice({
+  name: 'comments',
+  initialState: {
+    // Add initial state properties here.
+  },
+  // Add extraReducers here.
+});
+
+export const selectComments = (state) => state.comments.byArticleId;
+export const isLoadingComments = (state) => state.comments.isLoadingComments;
+export const createCommentIsPending = (state) => state.comments.createCommentIsPending;
+
+export default commentsSlice.reducer;
